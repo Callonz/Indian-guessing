d3b1e58a80f18226bad905a3885aa5b28faca60f
Pascal <callonz@protonmail.com>
2020-04-06 05:24:21 +0200

    minor code changes

--- indian-guessing.py
+++ indian-guessing.py
@@ -1,16 +1,16 @@
 while(1):
     print("Let's play an Indian guessing game!")
     playerguess = float(input("> "))
-    guess = 500000
+    guess = 500000.0
     allowedDivert = 0.001
     lastChange = 500000.0
     guessed = 0
     for round in range(1,31, 1):
         if guess > playerguess:
-            print(str(round) + " I Guess: " + str(guess) + "   Too big!")
+            print(str(round) + " I guess: " + str(guess) + "   Too big!")
             guess -= lastChange /2
         else:
-            print(str(round) + " I Guess: " + str(guess) + "   Too small!")
+            print(str(round) + " I guess: " + str(guess) + "   Too small!")
             guess += lastChange / 2
         lastChange = lastChange / 2
 
